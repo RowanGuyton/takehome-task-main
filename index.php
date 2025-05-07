b9a800d59e92c40215d14d74703d85c16f4c120e
Rowan Guyton <rlguyton91@gmail.com>
2025-05-07 11:54:39 +0100

    Refactors index.php, Adds code to dynamically generate article list.

--- index.php
+++ index.php
@@ -16,66 +16,85 @@ use App\App;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$app = new App();
-
-echo "<head>
-<link rel='stylesheet' href='http://design.wikimedia.org/style-guide/css/build/wmui-style-guide.min.css'>
-<link rel='stylesheet' href='styles.css'>
-<script src='main.js'></script>
-</head>";
-
-$title = '';
-$body = '';
-if ( isset( $_GET['title'] ) ) {
-	$title = htmlentities( $_GET['title'] );
-	$body = $app->fetchArticle( $_GET );
-	$body = file_get_contents( sprintf( 'articles/%s', $title ) );
-}
-
-$wordCount = wfGetWc();
-echo "<body>";
-echo "<div id=header class=header>
-<a href='/'>Article editor</a><div>$wordCount</div>
-</div>";
-echo "<div class='page'>";
-echo "<div class='main'>";
-echo "<h2>Create/Edit Article</h2>
-<p>Create a new article by filling out the fields below. Edit an article by typing the beginning of the title in the title field, selecting the title from the auto-complete list, and changing the text in the textfield.</p>
-<form action='index.php' method='post'>
-<input name='title' type='text' placeholder='Article title...' value=$title>
-<br />
-<textarea name='body' placeholder='Article body...' >$body</textarea>
-<br />
-<a class='submit-button' href='#' />Submit</a>
-<br />
-<h2>Preview</h2>
-$title\n\n
-$body
-<h2>Articles</h2>
-<ul>
-<li><a href='index.php?title=Foo'>Foo</a></li>
-</ul>
-</form>";
-
-if ( $_POST ) {
-	$app->saveArticle( sprintf( "articles/%s", $_POST['title'] ), $_POST['body'] );
-}
-echo "</div>";
-echo "</div>";
-echo "</body";
-
-function wfGetWc() {
+// Get word count for current article
+function wfGetWc()
+{
 	global $wgBaseArticlePath;
 	$wgBaseArticlePath = 'articles/';
 	$wc = 0;
-	$dir = new DirectoryIterator( $wgBaseArticlePath );
-	foreach ( $dir as $fileinfo ) {
-		if ( $fileinfo->isDot() ) {
+	$dir = new DirectoryIterator($wgBaseArticlePath);
+	foreach ($dir as $fileinfo) {
+		if ($fileinfo->isDot()) {
 			continue;
 		}
-		$c = file_get_contents( $wgBaseArticlePath . $fileinfo->getFilename() );
-		$ch = explode( " ", $c );
-		$wc += count( $ch );
+		$c = file_get_contents($wgBaseArticlePath . $fileinfo->getFilename());
+		$ch = explode(" ", $c);
+		$wc += count($ch);
 	}
 	return "$wc words written";
 }
+
+// Instantiate our app class
+$app = new App();
+
+// Retrieve all current articles
+$articles = $app->getCurrentArticles();
+
+// Set necessary variables
+$title = '';
+$body = '';
+if (isset($_GET['title'])) {
+	$title = htmlentities($_GET['title']);
+	$body = $app->fetchArticle($_GET);
+	$body = file_get_contents(sprintf('articles/%s', $title));
+}
+
+$wordCount = wfGetWc();
+
+?>
+
+<head>
+	<link rel='stylesheet' href='http://design.wikimedia.org/style-guide/css/build/wmui-style-guide.min.css'>
+	<link rel='stylesheet' href='styles.css'>
+	<script src='main.js'></script>
+</head>
+
+<body>
+	<div id=header class=header>
+		<a href='/'>Article editor</a>
+		<div><?php htmlspecialchars($wordCount) ?></div>
+	</div>
+	<div class='page'>
+		<div class='main'>
+			<h2>Create/Edit Article</h2>
+			<p>Create a new article by filling out the fields below. Edit an article by typing the beginning of the title in the title field, selecting the title from the auto-complete list, and changing the text in the textfield.</p>
+			<form action='index.php' method='post'>
+				<input name='title' type='text' placeholder='Article title...' value=<?php htmlspecialchars($title) ?>>
+				<br />
+				<textarea name='body' placeholder='Article body...'><?php htmlspecialchars($body) ?></textarea>
+				<br />
+				<a class='submit-button' href='#' />Submit</a>
+				<br />
+				<h2>Preview</h2>
+				<?php htmlspecialchars($title) ?>
+				<?php htmlspecialchars($body) ?>
+				<h2>Articles</h2>
+				<ul>
+
+					<?php foreach ($articles as $article): ?>
+						<li>
+							<a href="index.php?title=<?= urlencode($article) ?>">
+								<?= htmlspecialchars($article) ?>
+							</a>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+			</form>
+
+			<?php if ($_POST) {
+				$app->saveArticle(sprintf("articles/%s", $_POST['title']), $_POST['body']);
+			}
+			?>
+		</div>
+	</div>
+</body>
