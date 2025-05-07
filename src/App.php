1a26e546cd9c0411a21544e6d8303040c1717c4e
Rowan Guyton <rlguyton91@gmail.com>
2025-05-07 12:02:59 +0100

    Tidies code

--- src/App.php
+++ src/App.php
@@ -4,35 +4,40 @@ namespace App;
 
 // TODO: Improve the readability of this file through refactoring and documentation.
 
-require_once dirname( __DIR__ ) . '/globals.php';
+require_once dirname(__DIR__) . '/globals.php';
 
 /*
 Basic class used for saving, updating, fetching individual articles,
 and retrieving all current articles
 */
-class App {
+class App
+{
 
 	// Saves POSTED article
-	public function saveArticle( $title, $body ) {
-		error_log( "Saving article $title, success!" );
-		file_put_contents( $title, $body );
+	public function saveArticle($title, $body)
+	{
+		error_log("Saving article $title, success!");
+		file_put_contents($title, $body);
 	}
 
 	// Updates selected article
-	public function updateArticle( $title, $body ) {
-		$this->saveArticle( $title, $body );
+	public function updateArticle($title, $body)
+	{
+		$this->saveArticle($title, $body);
 	}
 
 	// Fetches individual article
-	public function fetchArticle( $get ) {
+	public function fetchArticle($get)
+	{
 		$title = $get['title'] ?? null;
-		return is_array( $get ) ? file_get_contents( sprintf( 'articles/%s', $get['title'] ) ) :
-			file_get_contents( sprintf( 'articles/%s', $_GET['title'] ) );
+		return is_array($get) ? file_get_contents(sprintf('articles/%s', $get['title'])) :
+			file_get_contents(sprintf('articles/%s', $_GET['title']));
 	}
 
 	// Retrieves all current articles
-	public function getCurrentArticles() {
+	public function getCurrentArticles()
+	{
 		global $wgBaseArticlePath;
-		return array_diff( scandir( $wgBaseArticlePath ), [ '.', '..', '.DS_Store' ] );
+		return array_diff(scandir($wgBaseArticlePath), ['.', '..', '.DS_Store']);
 	}
 }
