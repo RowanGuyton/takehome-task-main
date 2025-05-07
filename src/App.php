52454ebb9acbc0a4f2faa07425158fb7803b1653
Rowan Guyton <rlguyton91@gmail.com>
2025-05-07 10:16:40 +0100

    Updates function names to improve readability in App.php

--- src/App.php
+++ src/App.php
@@ -8,22 +8,22 @@ require_once dirname( __DIR__ ) . '/globals.php';
 
 class App {
 
-	public function save( $title, $body ) {
+	public function saveArticle( $title, $body ) {
 		error_log( "Saving article $title, success!" );
 		file_put_contents( $title, $body );
 	}
 
-	public function update( $title, $body ) {
-		$this->save( $title, $body );
+	public function updateArticle( $title, $body ) {
+		$this->saveArticle( $title, $body );
 	}
 
-	public function fetch( $get ) {
+	public function fetchArticle( $get ) {
 		$title = $get['title'] ?? null;
 		return is_array( $get ) ? file_get_contents( sprintf( 'articles/%s', $get['title'] ) ) :
 			file_get_contents( sprintf( 'articles/%s', $_GET['title'] ) );
 	}
 
-	public function getListOfArticles() {
+	public function getCurrentArticles() {
 		global $wgBaseArticlePath;
 		return array_diff( scandir( $wgBaseArticlePath ), [ '.', '..', '.DS_Store' ] );
 	}
