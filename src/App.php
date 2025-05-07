436a6fafd39bfa56015aa00bdb15bce640904506
Rowan Guyton <rlguyton91@gmail.com>
2025-05-07 10:23:24 +0100

    Adds documentation for App class and individual functions

--- src/App.php
+++ src/App.php
@@ -6,23 +6,31 @@ namespace App;
 
 require_once dirname( __DIR__ ) . '/globals.php';
 
+/*
+Basic class used for saving, updating, fetching individual articles,
+and retrieving all current articles
+*/
 class App {
 
+	// Saves POSTED article
 	public function saveArticle( $title, $body ) {
 		error_log( "Saving article $title, success!" );
 		file_put_contents( $title, $body );
 	}
 
+	// Updated selected article
 	public function updateArticle( $title, $body ) {
 		$this->saveArticle( $title, $body );
 	}
 
+	// Fetches individual article
 	public function fetchArticle( $get ) {
 		$title = $get['title'] ?? null;
 		return is_array( $get ) ? file_get_contents( sprintf( 'articles/%s', $get['title'] ) ) :
 			file_get_contents( sprintf( 'articles/%s', $_GET['title'] ) );
 	}
 
+	// Retrieves all current articles
 	public function getCurrentArticles() {
 		global $wgBaseArticlePath;
 		return array_diff( scandir( $wgBaseArticlePath ), [ '.', '..', '.DS_Store' ] );
